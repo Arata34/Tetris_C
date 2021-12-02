d82341ae7ab55908b1a230f4c981bcb7e29e4ff6
OMOREAU <moreauolivia49@gmail.com>
2021-12-02 13:35:09 +0100

    piece OK

--- TetrisApp/Program.cs
+++ TetrisApp/Program.cs
@@ -2,27 +2,47 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using TetrisApp.Data;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+namespace TetrisApp
 {
-    app.UseExceptionHandler("/Error");
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Piece piece = new Piece();
+            Piece piece2 = new Piece();
+            Piece piece3= new Piece();
+            Piece piece4 = new Piece();
+
+
+            Console.WriteLine(piece.printPiece(piece.thisPiece));
+            Console.WriteLine(piece.printPiece(piece.RotatePiece()));
+
+
+        }
+    }
 }
 
+// var builder = WebApplication.CreateBuilder(args);
 
-app.UseStaticFiles();
+// // Add services to the container.
+// builder.Services.AddRazorPages();
+// builder.Services.AddServerSideBlazor();
+// builder.Services.AddSingleton<WeatherForecastService>();
 
-app.UseRouting();
+// var app = builder.Build();
 
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
+// // Configure the HTTP request pipeline.
+// if (!app.Environment.IsDevelopment())
+// {
+//     app.UseExceptionHandler("/Error");
+// }
 
-app.Run();
+
+// app.UseStaticFiles();
+
+// app.UseRouting();
+
+// app.MapBlazorHub();
+// app.MapFallbackToPage("/_Host");
+
+// app.Run();
